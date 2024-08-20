ca9a05dc8b41a269c97f258c9f1225c15878a95e
Sean Browning <25888380+beansrowning@users.noreply.github.com>
2024-08-20 17:18:00 -0400

    Packaging

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt", encoding="utf-8") as f:
+    requirements = f.read().splitlines()
+
+with open("README.md", encoding="utf-8") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name='NeuralNLP',
+    version='0.1',
+    description='A packaged version of the NeuralNLP Neural Classifier repo',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.9",
+    url='https://github.com/beansrowning/NeuralNLP-NeuralClassifier',
+    author='Sean Browning',
+    author_email='sbrowning@cdc.gov',
+    license='MIT',
+    packages=find_packages(include=["neural_nlp", "neural_nlp.*"]),
+    install_requires=requirements,
+    zip_safe=False
+)
